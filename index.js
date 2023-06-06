5d10b96da04426e16402250fbb88843253c22ed5
Sayan-K-Dutta <112009946+Sayan-K-Dutta@users.noreply.github.com>
2023-06-07 00:34:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKVIE. This is AWS Trial')
+  res.send('Hello MCKVIE. This is AWS Trial hello')
 })
 
 app.listen(4000, ()=>{
